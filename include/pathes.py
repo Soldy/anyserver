c630093ba6c202f604db7b2462cdfaa7d8fb88a2
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-23 20:05:53 +0000

    all function
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/pathes.py
+++ include/pathes.py
@@ -2,42 +2,53 @@ from copy import deepcopy
 from sys import argv
 import os
 import json
-import logging
 
-_file_  = 'pathes.json'
-_serial_ = 0
-_path_ = {}
-_log_level_   = logging.INFO
 
-def check()->bool:
-    _error = False
-    if not os.path.exists(_file_):
-        logging.info('Creating index file')
-        with open(_file_, 'w') as file_:
-            json.dump({}, file_)
-    if not os.path.isfile(_file_):
-        logging.critical('Path file error')
-        return True
-    return False
+class PathesClass:
+    def __init__(self, logging_, config_):
+        self._log    = logging_
+        self._config = config_
+        self._serial = 0
+        self._path   = {}
+    def check(self)->bool:
+        _error = False
+        if not os.path.exists(
+          self._config['path'],
+        ):
+            self._log.info('Creating index file')
+            with open(
+              self._config['path'],
+              'w'
+            ) as file_:
+                json.dump({}, file_)
+        if not os.path.isfile(
+          self._config['path'],
+        ):
+            self._log.critical('Path file error')
+            return True
+        return False
+     def save(self):
+         with open(
+           self._config['path'], 
+           'w'
+         ) as file_:
+             json.dump(self._path, file_)
 
-def save():
-    global _path_
-    with open(_file_, 'w') as file_:
-        json.dump(_path_, file_)
-
-def read():
-    global _path_
-    with open(_file_, 'r') as file_:
-        _path_ = json.load(file_)
-
-def add(path_:str)->int:
-    global _path_
-    global _serial_
-    if path_ not in _path_:
-        _serial_ = deepcopy(_serial_ + 1)
-        _path_[path_] = _serial_
-        save()
-    return _path_[path_]
-
-def get(path_:str)->int:
-    return add(path_)
+    def read(self):
+        with open(
+          self._config['path'],
+          'r'
+        ) as file_:
+            self._path = json.load(file_)
+    def add(self, path_:str)->int:
+        if path_ not in self._path:
+            self._serial = deepcopy(
+              self._serial + 1
+            )
+            self. _path[path_] = self._serial
+            self.save()
+        return self._path[path_]
+    def get(path_:str)->int:
+        return self.add(path_)
+    def all(self):
+        return deepcopy(self._path)
