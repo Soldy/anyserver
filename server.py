b57e60d4422db113b01e5f3d0263f692a85627e7
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-27 12:29:06 +0000

    single file version
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- server.py
+++ server.py
@@ -8,214 +8,415 @@ import datetime
 import logging
 
 
-"""
-This is the temp config zone.
-This should be replaced by a config class or function. 
-"""
-_id_ = 0
-_index_file_ = 'index.json'
-_server_host_ = 'localhost'
-_server_port_ = 8008
-_data_base_ = {}
-_save_data_ = True
-_load_data_ = True
-_log_level_ = logging.INFO
-
-_fresh_db_ = False
-_index_ = []
-_db_dir_ = 'db'
-
-"""
-W3C Date string generator
-
-
-This is just a temporary solution.
-Storing a date string or working with it is stupid.
-Have to change to unix timestamp.
-
-:return: str : w3c
-"""
-def dateTimeNow()->str:
-    return (
-        datetime.
-        datetime.
-        now().
-        replace(tzinfo=datetime.timezone.utc).
-        isoformat()
-    )
-
-
-
-""" This checking the file system for initialization. """
-def _dbExistCheck():
-    _error = False
-    if not _save_data_ and not _load_data_:
-        return 
-    if not os.path.exists(_db_dir_):
-        logging.info('Creating database directory')
-        os.makedirs(_db_dir_)
-        _fresh_db_ = True 
-    if not os.path.isdir(_db_dir_):
-        logging.critical('Database directory error')
-        _error = True
-    if not os.path.exists(_index_file_):
-        logging.info('Creating index file')
-        with open(_index_file_, 'w') as file_:
-            json.dump([], file_)
-        _fresh_db_ = True 
-    if not os.path.isfile(_index_file_):
-        logging.critical('Index file error')
-        _error = True
-    if _error:
-          quit()
+_config = {
+    "port"   : 8008,
+    "host"   : "localhost",
+    "db_dir" : "db",
+    "index"  : "indexes.json",
+    "path"   : "pathes.json",
+    "log"    : logging.DEBUG,
+    "load"   : True,
+    "save"   : True
+}
 
 
 """
-Db record file name 
-
-:param: str : the record id in str
-:return: str: full path 
+Multipath manager class.
 """
-def _fileName(id_:str)->str:
-    return (
-        _db_dir_+
-        '/'+
-        str(id_)+
-        '.json'
-    )
+class PathesClass:
+    def __init__(self, logging_, config_):
+        self._log    = logging_
+        self._config = config_
+        self._serial = 0
+        self._path   = {}
+        self.check()
+
+    """
+    Path cache check
+    """
+    def check(self)->bool:
+        _error = False
+        if not self._config['load']:
+            return False
+        if not self._config['save']:
+            return False
+        if not os.path.exists(
+          self._config['path'],
+        ):
+            self._log.info('Creating index file')
+            with open(
+              self._config['path'],
+              'w'
+            ) as file_:
+                json.dump({}, file_)
+        if not os.path.isfile(
+          self._config['path'],
+        ):
+            self._log.critical('Path file error')
+            return True
+        return False
+
+    """
+    Path cache save
+    """
+    def save(self):
+        if not self._config['save']:
+            return
+        with open(
+          self._config['path'],
+          'w'
+        ) as file_:
+           json.dump(self._path, file_)
+
+    """
+    Path cache load 
+    """
+    def load(self):
+        if not self._config['load']:
+            return
+        self._log.debug(
+          'Loading patheses file'
+        )
+        with open(
+          self._config['path'],
+          'r'
+        ) as file_:
+            self._path = json.load(file_)
+    def add(self, path_:str)->str:
+        if path_ not in self._path:
+            self._serial = self._serial + 1
+            self. _path[path_] = deepcopy(self._serial)
+            self.save()
+        return deepcopy(str(self._path[path_]))
+    def get(self, path_:str)->str:
+        return self.add(path_)
+    def all(self):
+        return deepcopy(self._path)
+
 
 
 """
-Db record save 
-
-:param: str : the record id in str
+Index class.
 """
-def _dbSave(id_:str):
-    global _data_base_
-    if _save_data_ == True:
-        with open(_fileName(id_), 'w') as file_:
-            json.dump(_data_base_[id_], file_)
+class IndexesClass:
+    def __init__(self, logging_, config_):
+        self._log = logging_
+        self._config = config_
+        self._ids = {}
+        self._index = {}
 
-"""
-Db record load
+    """
+    Index cache check
+    """
+    def check(self)->bool:
+        _file_ = self._config['index']
+        _error = False
+        if not self._config['load']:
+            return False
+        if not self._config['save']:
+            return False
+        if not os.path.exists(_file_):
+            self._log.info('Creating index file')
+            with open(_file_, 'w') as file_:
+                json.dump({}, file_)
+        if not os.path.isfile(_file_):
+            self._log.critical('Index file error')
+            return True
+        return False
 
-:param: str : the record id in str
-"""
-def _dbRead(id_:str):
-    global _data_base_
-    with open(_fileName(id_), 'r') as file_:
-        _data_base_[id_] = json.load(file_)
+    """
+    Index cache save
+    """
+    def save(self):
+        if not self._config['save']:
+            return
+        _file_ = self._config['index']
+        with open(self._config['index'], 'w') as file_:
+            json.dump(self._index, file_)
 
-def _dbReadAll():
-    global _index_
-    for i in _index_:
-        _dbRead(i)
-
-def _indexSave():
-    global _index_
-    with open(_index_file_, 'w') as file_:
-        json.dump(_index_, file_)
-
-def _indexRead():
-    global _index_
-    with open(_index_file_, 'r') as file_:
-         _index_ = json.load(file_)
-
-def _indexAdd(id_:str)->int:
-    global _index_
-    if id_ not in _index_:
-        _index_.append(id_)
-    _indexSave()
-
-def _indexDel(id_:str)->int:
-    global _index_
-    del _index_[_index_.index(id_)]
-    _indexSave()
-
-def _findId():
-    global _index_
-    global _id_
-    for i in  _index_:
-        if int(i) > _id_:
-            _id_ = deepcopy(int(i))
-
-def _addId()->str:
-    global _id_
-    _id_ = _id_ + 1
-    return deepcopy(str(_id_))
-
-
-def _postSave_(data_: dict[str, str]):
-    global _data_base_
-    now = dateTimeNow()
-    _id = _addId()
-    data_['id'] = _id
-    data_['is_active'] = True
-    data_['created_at'] = deepcopy(now)
-    data_['updated_at'] = deepcopy(now)
-    _data_base_[_id] = data_
-    _indexAdd(_id)
-    _dbSave(_id)
-
-def _postEdit_(data_: dict[str,str]):
-    _id = str(data_['id'])
-    if 'id' not in data_:
-        return
-    if _id not in _data_base_:
-        return
-    if _data_base_[_id]['is_active'] == False:
-        return
-    data_['is_active'] = True
-    data_['created_at'] = _data_base_[_id]['created_at']
-    data_['updated_at'] = dateTimeNow()
-    _data_base_[_id] = data_
-    _dbSave(_id)
+    """
+    Index cache load 
+    """
+    def load(self):
+        if not self._config['load']:
+            return
+        self._log.debug(
+          'Loading indexes file'
+        )
+        with open(self._config['index'], 'r') as file_:
+            self._index = json.load(file_)
+        for path in self._index:
+            if path not in self._ids:
+                self._ids[path] = 0
+            for i in self._index[path]:
+                if int(i) > self._ids[path]:
+                    self._ids[path] = int(i)
+    def add(self, path_:str, id_:str):
+        if path_ not in self._index:
+            self._index[path_] = []
+        if id_ not in self._index:
+            self._index[path_].append(deepcopy(id_))
+            self.save()
+    def addId(self, path_:str)->str:
+        if path_ not in self._ids:
+            self._ids[path_] = 0
+        self._ids[path_] = self._ids[path_] + 1
+        return deepcopy(str(self._ids[path_]))
+    def all(self, path_:str):
+        if path_ not in self._index:
+            return []
+        return deepcopy(self._index[path_])
 
 
-def _delete_(ids_ : list[int]):
-    for i in range(len(ids_)):
-         if str(ids_[i]) in _data_base_:
-             _data_base_[str(ids_[i])]['is_active'] = False
+class DatabasesClass:
+    def __init__(self, logging_, config_):
+        self._log = logging_
+        self._config = config_
+        self._db = {}
+        self._indexes = IndexesClass(
+          self._log,
+          self._config
+        )
+        self._patheses = PathesClass(
+          self._log,
+          self._config
+        )
+        self.check()
+        self._patheses.load()
+        self._indexes.load()
+        self.loadAll()
+    """
+    path name fix
+    """
+    def _pathFix(self, path_:str)->str:
+        return path_.replace("/", "_")
+    """ This checking the file system for initialization. """
+    def check(self):
+        _error = False
+        if not self._config["save"]:
+            return
+        if not self._config["load"]:
+            return
+        if not os.path.exists(
+          self._config["db_dir"]
+        ):
+            self._log.info(
+              'Creating database directory'
+            )
+            os.makedirs(
+              self._config["db_dir"]
+            )
+        if not os.path.isdir(
+          self._config["db_dir"]
+        ):
+            self._log.critical(
+              'Database directory error'
+            )
+            _error = True
+        if self._patheses.check():
+            _error = True
+        if self._indexes.check():
+            _error = True
+        if _error :
+            quit()
 
-def _getCopy_(ids_:list[str]):
-    out = []
-    for i in ids_:
-        if _data_base_[i]['is_active']:
-             out.append(deepcopy(_data_base_[i]))
-             out[len(out)-1]['is_active'] = 'true'
-    return out
-def _getAll_():
-    global _data_base_
-    return _getCopy_(_data_base_)
+    """
+    Db record file name 
 
-def _getId_(ids_):
-    global _data_base_
-    out = {}
-    for i in ids_:
-        if str(i) in _data_base_:
-            out[str(i)] = _data_base_[str(i)]
-    return _getCopy_(out)
+    :param: str : the record id in str
+    :return: str: full path 
+    """
+    def _fileName(self, path_: str, id_: str)->str:
+        return (
+          self._config["db_dir"]+
+          '/'+
+          path_+
+          '_'+
+          str(id_)+
+          '.json'
+        )
+    """
+    Db record load
 
-def _getFilter_(filters_: dict[str,str]):
-    global _data_base_
-    out = {}
-    for a in _data_base_:
-        for b in filters_:
-            if b in _data_base_[a]:
-                for c in filters_[b]:
-                    if c in _data_base_[a][b]:
-                        out[a] = _data_base_[a]
-    return _getCopy_(out)
+    :param: str : the record id in str
+    """
+    def load(self, path_: str, id_: str):
+        if not self._config["load"]:
+            return
+        if path_ not in self._db:
+            self._db[path_] = {}
+        with open(self._fileName(path_, id_), 'r') as file_:
+            self._db[path_][id_] = json.load(file_)
+    """
+    load all db records
 
+    :param: dict[str, dict[str, str]] index
+    """
+    def loadAll(self):
+        for path in self._patheses.all():
+            path = self._patheses.get(
+              path
+            )
+            for id_ in self._indexes.all(
+              path
+            ):
+                self.load(path, id_)
+    """
+    Db record save
+
+    :param: str : the record id in str
+    """
+    def save(self, path_: str, id_:str):
+        if self._config['save'] == True:
+            with open(
+              self._fileName(path_, id_),
+              'w'
+            ) as file_:
+                json.dump(self._db[path_][id_], file_)
+
+    """
+    set record value
+
+    :param: str : the record path
+    :param: str : the record id in str
+    :param: dict[str,any] : data
+    """
+    def _set(self, path_: str, id_: str, data_: dict[str,any]):
+        if path_ not in self._db :
+            self._db[path_] = {}
+        self._db[path_][id_] = {}
+        data_['id'] = deepcopy(id_)
+        self._db[path_][id_]['data'] = deepcopy(data_)
+        self._db[path_][id_]['id'] = deepcopy(id_)
+        self._indexes.add(path_, id_)
+        self.save(path_, id_)
+
+    """
+    Db record post
+
+    :param: str : the record id in str
+    :return: int : result code 0 ok
+    """
+    def post(self, path_: str, data_: dict[str, str])->int:
+        path = self._patheses.get(
+          self._pathFix(path_)
+        )
+        _id = self._indexes.addId(
+          self._patheses.get(
+            self._pathFix(path_)
+          )
+        )
+        self._set(path_, _id, data_)
+        return 0
+    """
+    Db record post
+     result codes:
+        0 - O.K.
+        1 - missing id (invalid request)
+        2 - unkown path 
+        3 - unkown id
+
+    :param: str : the record id in str
+    :return: int : result code 0 ok
+    """
+    def patch(self, path_: str, data_: dict[str, str])->int:
+        path = self._patheses.get(
+          self._pathFix(path_)
+        )
+        if 'id' not in data:
+            return 1
+        _id = data_['id']
+        if path not in self._db:
+            return 2
+        if _id not in self._db[path]:
+            return 3
+        self._set(path_, data_['id'], data_)
+        return 0
+
+    """
+    get result element copy
+
+    :param:  str : element path
+    :param:  list[str] : element list
+    :return: list[dict[str,any]] : result element copy
+    """
+    def _getCopy(self, path_:str, ids_:list[str])->list[dict[str,any]]:
+        out = []
+        for i in ids_:
+            if i in self._db[path_]:
+                pack = deepcopy(self._db[path_][i]['data'])
+                pack['id'] = deepcopy(self._db[path_][i]['id'])
+                out.append(deepcopy(pack))
+        return out
+
+    """
+    get All record
+
+    :param: str : path
+    """
+    def _getAll(self, path_:str):
+        return self._getCopy(
+          path_,
+          self._db[
+            path_
+          ]
+        )
+    """
+    get records by id
+
+    :param: str : path
+    :param: list[str] : id list
+    """
+    def _getId(self, path_: str, ids_: list[str]):
+        out = []
+        for i in ids_:
+            if str(i) in self._db[path_]:
+                out.append(str(i))
+        return self._getCopy(path_, out)
+    """
+    get elements by filter
+
+    :param: str : path
+    :param: dict[str, str] : filters
+    """
+    def _getFilter(self, path_: str, filters_: dict[str,str]):
+        out = []
+        for a in self._db[path_]:
+            for b in filters_:
+                if b in self._db[path_][a]['data']:
+                   for c in filters_[b]:
+                       if c in self._db[path_][a]['data'][b]:
+                            out.append(str(a))
+        return self._getCopy(path_, out)
+    """
+    get request manager
+
+    :param: str : the record id in str
+    """
+    def get(self, path_: str, gets_: dict[str,str]):
+        path = self._patheses.get(
+          self._pathFix(path_)
+        )
+        if path not in self._db:
+            return {}
+        if 'id' in gets_:
+            return self._getId(path, gets_['id'])
+        if gets_ == {}:
+            return self._getAll(path)
+        else:
+            return self._getFilter(path, gets_)
 
 
 
 class Server(BaseHTTPRequestHandler):
-    def _get_clearpath(self):
+    def __init__(self, *args):
+        self._logging = logging
+        BaseHTTPRequestHandler.__init__(self, *args)
+    def _clearPath(self)->str:
         if '?' not in self.path:
            return deepcopy(self.path)
         return deepcopy(path[:path.index('?')])
-    def _get_variables(self):
+    def _getVariables(self)->dict[str,str]:
         out = {}
         if '?' not in self.path:
            return out
@@ -234,7 +435,7 @@ class Server(BaseHTTPRequestHandler):
                    out[key] = []
                 out[key].append(value)
         return out
-    def _do_response(self, data_: dict[str, any]):
+    def _do_response(self, data_: str):
         out = data_.encode()
         self.protocol_version = 'HTTP/1.1'
         self.send_response(200)
@@ -243,76 +444,63 @@ class Server(BaseHTTPRequestHandler):
         self.send_header('Content-length', len(out))
         self.end_headers()
         self.wfile.write(out)
-    def _do_json_response(self, data_: dict[str, any]):
+    def _do_json_response(self, data_: dict[str, any] | list[dict[str, any]]):
         return self._do_response(
             json.dumps(
                 data_
             )
         )
     def do_GET(self):
-        gets = self._get_variables()
-        if 'id' in gets:
-            return self._do_json_response(
-                _getId_(gets['id'])
-            )
-        if gets == {}:
-            return self._do_json_response(
-                _getAll_()
-            )
-        else:
-            return self._do_json_response(
-                _getFilter_(gets)
-            )
+        self._do_json_response(
+          _db.get(
+            self._clearPath(),
+            self._getVariables()
+          )
+        )
     def do_POST(self):
         length = int(self.headers['content-length'])
         field = self.rfile.read(length).decode()
         post_data = json.loads(field)
-        _postSave_(post_data)
-        self._do_response(json.dumps('{}'))
-    def do_DELETE(self):
-        length = int(self.headers['content-length'])
-        field = self.rfile.read(length).decode()
-        post_data = json.loads(field)
-        _delete_(post_data)
+        _db.post(
+          self._clearPath(),
+          post_data
+        )
         self._do_response(json.dumps('{}'))
     def do_PATCH(self):
         length = int(self.headers['content-length'])
         field = self.rfile.read(length).decode()
         post_data = json.loads(field)
-        _postEdit_(post_data)
+        database.path(post_data)
         self._do_response(json.dumps('{}'))
     def log_message(self, format, *args: list[str]):
         if len(args) == 3:
-            logging.info(args[0]+" "+args[1]+" "+args[2])
+            self._logging.info(args[0]+" "+args[1]+" "+args[2])
             return
         out = ''
         for i in args:
             out = out + str(i)
-        logging.info(out)
+        self._logging.info(out)
         return
-
 
 def _httpServer(server_class=HTTPServer, handler_class=Server, port=8008, host='127.0.0.1', protocol_version='HTTP/1.1'):
     server_address = (host, port)
     httpd = server_class(server_address, handler_class, protocol_version)
-    logging.info('httpd starting')
+    logging.debug('httpd starting')
     httpd.serve_forever()
+
+def start():
+    global _config
+    _httpServer(host=_config['host'], port=_config['port'])
 
 """
 server init
 """
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=_log_level_)
+logging.basicConfig(
+  format='%(asctime)s - %(levelname)s - %(message)s',
+  level=_config['log']
+)
 
-_dbExistCheck()
-
-if _load_data_ and not _fresh_db_:
-    _indexRead()
-    _dbReadAll()
-    _findId()
-
-
-
-   
+_db = DatabasesClass(logging, _config)
 
 if __name__ == "__main__":
-        _httpServer(host=_server_host_, port=_server_port_)
+    start()
