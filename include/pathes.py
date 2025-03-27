afdf07d751f96d9a11211b7613546e2ba18a673f
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-27 11:34:54 +0000

    path manager
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/pathes.py
+++ include/pathes.py
@@ -4,14 +4,26 @@ import os
 import json
 
 
+"""
+Multipath manager class.
+"""
 class PathesClass:
     def __init__(self, logging_, config_):
         self._log    = logging_
         self._config = config_
         self._serial = 0
         self._path   = {}
+        self.check()
+
+    """
+    Path cache check
+    """
     def check(self)->bool:
         _error = False
+        if not self._config['load']:
+            return False
+        if not self._config['save']:
+            return False
         if not os.path.exists(
           self._config['path'],
         ):
@@ -27,28 +39,40 @@ class PathesClass:
             self._log.critical('Path file error')
             return True
         return False
-     def save(self):
-         with open(
-           self._config['path'], 
-           'w'
-         ) as file_:
-             json.dump(self._path, file_)
 
-    def read(self):
+    """
+    Path cache save
+    """
+    def save(self):
+        if not self._config['save']:
+            return
+        with open(
+          self._config['path'],
+          'w'
+        ) as file_:
+           json.dump(self._path, file_)
+
+    """
+    Path cache load 
+    """
+    def load(self):
+        if not self._config['load']:
+            return
+        self._log.debug(
+          'Loading patheses file'
+        )
         with open(
           self._config['path'],
           'r'
         ) as file_:
             self._path = json.load(file_)
-    def add(self, path_:str)->int:
+    def add(self, path_:str)->str:
         if path_ not in self._path:
-            self._serial = deepcopy(
-              self._serial + 1
-            )
-            self. _path[path_] = self._serial
+            self._serial = self._serial + 1
+            self. _path[path_] = deepcopy(self._serial)
             self.save()
-        return self._path[path_]
-    def get(path_:str)->int:
+        return deepcopy(str(self._path[path_]))
+    def get(self, path_:str)->str:
         return self.add(path_)
     def all(self):
         return deepcopy(self._path)
