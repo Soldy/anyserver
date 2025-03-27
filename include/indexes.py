2b6c8a365c16e20c5c966366996c66497512c7ec
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-27 11:35:06 +0000

    index manager
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/indexes.py
+++ include/indexes.py
@@ -4,15 +4,26 @@ import os
 import json
 
 
+"""
+Index class.
+"""
 class IndexesClass:
     def __init__(self, logging_, config_):
         self._log = logging_
         self._config = config_
         self._ids = {}
         self._index = {}
+
+    """
+    Index cache check
+    """
     def check(self)->bool:
         _file_ = self._config['index']
         _error = False
+        if not self._config['load']:
+            return False
+        if not self._config['save']:
+            return False
         if not os.path.exists(_file_):
             self._log.info('Creating index file')
             with open(_file_, 'w') as file_:
@@ -21,24 +32,46 @@ class IndexesClass:
             self._log.critical('Index file error')
             return True
         return False
+
+    """
+    Index cache save
+    """
     def save(self):
+        if not self._config['save']:
+            return
         _file_ = self._config['index']
         with open(self._config['index'], 'w') as file_:
             json.dump(self._index, file_)
-    def read(self):
+
+    """
+    Index cache load 
+    """
+    def load(self):
+        if not self._config['load']:
+            return
+        self._log.debug(
+          'Loading indexes file'
+        )
         with open(self._config['index'], 'r') as file_:
             self._index = json.load(file_)
-
-    def add(self, path_:str, id_:str)->int:
+        for path in self._index:
+            if path not in self._ids:
+                self._ids[path] = 0
+            for i in self._index[path]:
+                if int(i) > self._ids[path]:
+                    self._ids[path] = int(i)
+    def add(self, path_:str, id_:str):
         if path_ not in self._index:
             self._index[path_] = []
         if id_ not in self._index:
             self._index[path_].append(deepcopy(id_))
             self.save()
-    def addId(self, path:str)->str:
+    def addId(self, path_:str)->str:
         if path_ not in self._ids:
-            self_ids[path_] = 0
+            self._ids[path_] = 0
         self._ids[path_] = self._ids[path_] + 1
         return deepcopy(str(self._ids[path_]))
-    def all(self):
-        return deepcopy(self._index)
+    def all(self, path_:str):
+        if path_ not in self._index:
+            return []
+        return deepcopy(self._index[path_])
