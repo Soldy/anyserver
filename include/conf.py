a02a761b314b8e4c8be627433ac90453f655042d
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-30 16:18:08 +0100

    simplified conf process
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ include/conf.py
@@ -0,0 +1,46 @@
+
+_config = {
+    "port"      : 8008,
+    "host"      : "localhost",
+    "db_dir"    : "db",
+    "index"     : "indexes.json",
+    "path"      : "pathes.json",
+    "log_level" : 10,
+    "load"      : True,
+    "save"      : True
+}
+
+def start (args, logging_)->dict[str,str]:
+    global _config
+    if int(str(int(args.port))) != args.port:
+       logging_.critical("invalid port")
+       quit()
+    if args.port > 65535:
+       logging_.critical("invalid port to big number")
+       quit()
+    if args.port < 1:
+       logging_.critical("invalid port to low number")
+       quit()
+    if int(str(int(args.log_level))) != args.log_level:
+       logging_.critical("invalid log level")
+       quit()
+    if args.log_level > 50:
+       logging_.critical("invalid log level to big number")
+       quit()
+    if args.log_level < 10:
+       logging_.critical("invalid log level low number")
+       quit()
+
+    _config["port"] = int(args.port)
+    _config["log_level"] = int(args.log_level)
+    _config["host"] = args.host
+    _config["db_dir"] = args.db
+    _config["index"] = args.index
+    _config["path"] = args.path
+    if args.load == False:
+        _config["load"] = False
+    if args.save == False:
+        _config["save"] = False
+    if args.vv:
+        _config["log_level"] = 10
+    return _config
