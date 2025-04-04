f1fd4051dcb8a613779dbe859787755b598e3975
Soldy <4786022+Soldy@users.noreply.github.com>
2025-04-04 12:32:59 +0100

    forward class empty base
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/forward.py
+++ include/forward.py
@@ -3,31 +3,41 @@ from copy import deepcopy
 
 
 class ForwarderClass():
-    def __init__(self, db_):
-        self._host = globForHost
+    def __init__(self, db_, logging_, config_):
         self._db = db_
+        self._config = config_
     def _headerProcess(self, headers):
         out = {}
         remove = ['host', 'content-length']
         for h in headers:
             if h.lower() not in remove: 
                 out[h] = headers[h]
-        out['HOST'] = self._host
+        out['HOST'] = self._config['forward']
         return out
     def forward(self, path, headers, method, body):
-       if method == 'POST':
-           self.post(
+       if self._config['forward'] == '':
+          return '{}'
+       if method == 'GET':
+           return self.post(
              path,
              self._headerProcess(headers)
+           )
+       if method == 'POST':
+           return self.post(
+             path,
+             self._headerProcess(headers),
              body
            )
 
     def get(self, path, headers):
        self._headerProcess(headers)
+       return '{}'
     def post(self, path, headers,body):
        conn = http.client.HTTPSConnection(host)
        conn.request("POST", path, body ,headers)
        response = conn.getresponse()
        self._headerProcess(headers)
+       return '{}'
     def path(self):
        self._headerProcess(headers)
+       return '{}'
