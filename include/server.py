d2193b46fc2012edf1f5c9de36956407b9bea821
Soldy <4786022+Soldy@users.noreply.github.com>
2025-05-02 23:27:03 +0100

    dbm server implementation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- include/server.py
+++ include/server.py
@@ -7,6 +7,7 @@ import json
 import forward
 import database
 from database import DatabasesClass
+from databasedbm import DatabasesDbmClass
 
 _db = ''
 _forward = ''
@@ -63,7 +64,7 @@ class Server(BaseHTTPRequestHandler):
             )
           )
         return self._do_json_response(result)
-          
+
     def do_POST(self):
         length = int(self.headers['content-length'])
         field = self.rfile.read(length).decode()
@@ -73,11 +74,13 @@ class Server(BaseHTTPRequestHandler):
           post_data
         )
         self._do_response(json.dumps({}))
+
     def do_PATCH(self):
         length = int(self.headers['content-length'])
         field = self.rfile.read(length).decode()
         post_data = json.loads(field)
         self._do_response(json.dumps({}))
+
     def log_message(self, format, *args: list[str]):
         if len(args) == 3:
             self._logging.info(args[0]+" "+args[1]+" "+args[2])
@@ -87,6 +90,7 @@ class Server(BaseHTTPRequestHandler):
             out = out + str(i)
         self._logging.info(out)
         return
+
 
 def _httpServer(logging_, server_class=HTTPServer, handler_class=Server, port=8008, host="127.0.0.1", protocol_version="HTTP/1.1"):
     server_address = (host, port)
@@ -98,10 +102,16 @@ def start(logging_, config_):
     global _db
     global _forward
     global _logging
-    _db = DatabasesClass(
-      logging_,
-      config_
-    )
+    if config_['store_type'] == 'json':
+        _db = DatabasesClass(
+          logging_,
+          config_
+        )
+    else:
+        _db = DatabasesDbmClass(
+          logging_,
+          config_
+        )
     _forward = forward.ForwarderClass(_db, logging_, config_)
     _logging = logging_
     _httpServer(logging_, host=config_["host"], port=config_["port"])
